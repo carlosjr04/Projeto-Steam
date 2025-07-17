a1aef6114af8df5200c06ae1d1b15057462031c0
AyrtonSur <ayrtonsurica@gmail.com>
2025-07-17 02:58:29 -0300

    fix: consertando problemas no modal do menu hamburguer

--- Frontend/src/components/GlobalComponents/Header/header.tsx
+++ Frontend/src/components/GlobalComponents/Header/header.tsx
@@ -6,6 +6,7 @@ import { Link, useNavigate } from 'react-router-dom'
 import { useCarrinhoStore } from '../../../store/useCarrinhoStore'
 import { useAuthStore } from '../../../store/authStore'
 import { useGetUserId } from '../../../hooks/User/useGetUser';
+import HamburguerModal from './HamburguerModal';
 
 
 export default function Header() {
@@ -83,99 +84,14 @@ export default function Header() {
           </div>
         </div>
 
-        {menuOpen && (
-          <div className={`modal-backdrop fade show ${styles['modal-header-home']}`} style={{zIndex: 1040}} onClick={closeMenu} />
-        )}
-        {menuOpen && (
-          <div className={`modal d-block ${styles['modal-header-home']}`} tabIndex={-1} style={{zIndex: 1050}}>
-            <div className={`modal-dialog ${styles['modal-header-home']} position-absolute top-0 start-0`}>
-              <div className={styles['modal-content']} onClick={e => e.stopPropagation()} style={{position: 'relative'}}>
-                <button type="button" className="btn-close position-absolute end-0 m-2" aria-label="Fechar" onClick={closeMenu} style={{zIndex: 10}} />
-                <div className={styles["main-content-modal"]}>
-                  <div className={styles["modal-header"]}>
-                    {isAuthenticated ? (
-                      <Link
-                        to="/biblioteca"
-                        className="modal-title text-decoration-none h5"
-                        id="menuModalLabel"
-                        style={{ textAlign: "start", color: 'rgb(189, 189, 189)', marginLeft: '4px', fontWeight: '200', fontSize: '1.5rem' }}
-                        onClick={closeMenu}
-                      >
-                        {user?.name ? user.name : 'Minha Biblioteca'}
-                      </Link>
-                    ) : (
-                      <Link
-                        to="/login"
-                        className="modal-title text-decoration-none h5"
-                        id="menuModalLabel"
-                        style={{ textAlign: "start", color: 'rgb(189, 189, 189)', marginLeft: '4px', fontWeight: '200', fontSize: '1.5rem' }}
-                        onClick={closeMenu}
-                      >
-                        Iniciar sessão
-                      </Link>
-                    )}
-                  </div>
-                  <div className={styles["modal-body"]}>
-                    <ul className={styles["list-unstyled"]}>
-                      <li>
-                        <div className="dropdown">
-                          <a
-                            className="dropdown-toggle text-decoration-none"
-                            href="#"
-                            role="button"
-                            data-bs-toggle="dropdown"
-                            aria-expanded="false"
-                            onClick={e => e.preventDefault()}
-                          >
-                            Loja
-                          </a>
-                          <ul className="dropdown-menu dropdown-menu-dark">
-                            <li><Link className="dropdown-item" to="/inConstrution">Home</Link></li>
-                            <li><Link className="dropdown-item" to="/inConstrution">Discovery Queue</Link></li>
-                            <li><Link className="dropdown-item" to="/inConstrution">Wishlist</Link></li>
-                          </ul>
-                        </div>
-                      </li>
-                      <li><Link to="/inConstrution" className="text-decoration-none">Comunidade</Link></li>
-                      <li><Link to="/carrinho" className="text-decoration-none">{`Carrinho(${numJogos})`}</Link></li>
-                      <li><Link to="/about" className="text-decoration-none">Sobre</Link></li>
-                      <li><Link to="/inConstrution" className="text-decoration-none">Suporte</Link></li>
-                      {isAuthenticated && (
-                        <li>
-                          <a
-                            href="#"
-                            className="text-decoration-none"
-                            onClick={e => { e.preventDefault(); e.stopPropagation(); handleLogout(); }}
-                            style={{ textAlign: 'left', cursor: 'pointer', color: '#fff' }}
-                          >
-                            Sair
-                          </a>
-                        </li>
-                      )}
-                      <li>
-                        <Link to="/inConstrution" className="text-decoration-none">Alterar idioma</Link>
-                        <Link to="/inConstrution" className="text-decoration-none">Baixe o aplicativo móvel</Link>
-                        <Link to="/inConstrution" className="text-decoration-none">Ver versão para computadores</Link>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-                <div className={styles['footer-modal-home']}>
-                  © Valve Corporation. All rights reserved. All trademarks are property of their respective owners in the US and other countries.
-                  <span>
-                    <a href="https://store.steampowered.com/privacy_agreement/?snr=1_4_4__global-responsive-menu" target="_blank" rel="noreferrer">Privacy Policy</a>
-                    &nbsp;|&nbsp;
-                    <a href="http://www.valvesoftware.com/legal.htm" target="_blank" rel="noreferrer">Legal</a>
-                    &nbsp;|&nbsp;
-                    <a href="https://store.steampowered.com/subscriber_agreement/?snr=1_4_4__global-responsive-menu" target="_blank" rel="noreferrer">Steam Subscriber Agreement</a>
-                    &nbsp;|&nbsp;
-                    <a href="https://store.steampowered.com/steam_refunds/?snr=1_4_4__global-responsive-menu" target="_blank" rel="noreferrer">Refunds</a>
-                  </span>
-                </div>
-              </div>
-            </div>
-          </div>
-        )}
+      <HamburguerModal
+        isOpen={menuOpen}
+        onClose={closeMenu}
+        isAuthenticated={isAuthenticated}
+        userName={user?.name}
+        numJogos={numJogos}
+        handleLogout={handleLogout}
+      />
       </nav>
     </header>
   )
