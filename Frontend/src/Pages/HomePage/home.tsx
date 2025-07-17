273162139e47acb6952c03684b0d24422e7de786
AyrtonSur <ayrtonsurica@gmail.com>
2025-07-16 22:38:53 -0300

    feat: verificando se a pessoa está logada ou não

--- Frontend/src/Pages/HomePage/home.tsx
+++ Frontend/src/Pages/HomePage/home.tsx
@@ -1,11 +1,14 @@
+
 import { Link } from "react-router-dom";
 import CarrosselHome from "../../components/HomeComponents/CarrosselHome/carrosselHome";
 import CarrosselHomeSlide from "../../components/HomeComponents/CarrosselHomeSlide/CarrosselHomeSlide";
 import CarouselCategory from "../../components/HomeComponents/CategoriaCarrossel/CategoriaCarrossel";
 import CategoriaSlide from "../../components/HomeComponents/CategoriaSlide/CategoriaSlide";
 import style from "./style.module.css";
+import { useAuthStore } from "../../store/authStore";
 
 export default function Home() {
+  const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
   return (
     <div>
       <CarrosselHome />
@@ -16,18 +19,20 @@ export default function Home() {
       </div>
 
       <CategoriaSlide />
-      <div className={style["login-footer"]}>
-        <div className={style["login-div"]}>
-          <p>Iniciar sessão para ver recomendações personalizadas</p>
-          <Link className={style["button-login"]} to="/login">
-            <span>Iniciar sessão</span>
-          </Link>
-          <p>
-            Ou <Link to="/inConstrution">registre-se</Link> na Steam
-            gratuitamente.
-          </p>
+      {!isAuthenticated && (
+        <div className={style["login-footer"]}>
+          <div className={style["login-div"]}>
+            <p>Iniciar sessão para ver recomendações personalizadas</p>
+            <Link className={style["button-login"]} to="/login">
+              <span>Iniciar sessão</span>
+            </Link>
+            <p>
+              Ou <Link to="/cadastro">registre-se</Link> na Steam
+              gratuitamente.
+            </p>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
