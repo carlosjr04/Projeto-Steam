700230603f5d53119077972ad9861bedc1ee2ae9
AyrtonSur <ayrtonsurica@gmail.com>
2025-07-17 15:22:33 -0300

    feat: começando criação de modal para adicionar jogo

---
+++ Frontend/src/types/GameCreatePayload.ts
@@ -0,0 +1,16 @@
+export type GameCreatePayload = {
+  title: string;
+  preco: number;
+  cover: string;
+  desconto: number;
+  desenvolvedora: string;
+  classificacao: string[];
+  idiomas: number[];
+  compatibilidade: string[];
+  dataLancamento: string;
+  categories: number[];
+  about: string;
+  descricao: string;
+  scenes: string[];
+  exemplo: string[];
+};
